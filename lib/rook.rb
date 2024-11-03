8fc74caedb66274cea1196e6b0c009401d3db08f
Gurleen55 <gurleencan6@gmail.com>
2024-11-03 13:33:24 -0400

    add valid moves method

--- lib/rook.rb
+++ lib/rook.rb
@@ -7,17 +7,24 @@ class Rook
     @type = type
   end
 
-  def valid_moves
+  private
+
+  def valid_moves(player, board, direction, orientation)
     i, j = position
     moves = []
-
-    8.times do |x|
-      moves << [i, x]
+    counter = %w[up right].include?(direction) ? 1 : -1
+    loop do
+      # Adjust next_position based on orientation
+      next_position = orientation == :vertical ? [i += counter, j] : [i, j += counter]
+      moves << next_position
+      # Check if next_position is occupied
+      unless board.squares[next_position].nil?
+        moves.pop if board.squares.piece.type == player.piece_type
+        break
+      end
+      # Boundary check based on orientation
+      break unless orientation == :vertical ? next_position[0].between?(1, 6) : next_position[1].between?(1, 6)
     end
-    8.times do |x|
-      moves << [x, j]
-    end
-
-    moves.reject { |move| move == position }
+    moves
   end
 end
